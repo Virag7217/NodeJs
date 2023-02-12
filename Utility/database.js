9f03428a284b167e2bc3d80fa1318933423f48b3
virag ranipa <virag.ranipa@seaflux.tech>
2023-02-12 17:32:52 +0530

    hide connection

--- Utility/database.js
+++ Utility/database.js
@@ -1,14 +1,28 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db ;
+
 const mongoConnect = (callback) => {
     
-    MongoClient.connect('mongodb+srv://virag:mongodbforvirag7217@node.fq8v4eo.mongodb.net/?retryWrites=true&w=majority')
+    // MongoClient.connect('mongodb+srv://virag:mongodbforvirag7217@node.fq8v4eo.mongodb.net/shop?retryWrites=true&w=majority')
         .then(client => {
             console.log('Connected !!');
+            _db = client.db();
             callback(client);
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
         };
 
-module.exports = mongoConnect ; 
+const getDb = () => {
+    if(_db){
+        return _db;
+    }
+    throw 'No Database Found !';
+
+};
+exports.mongoConnect = mongoConnect ;
+exports.getDb = getDb ; 
