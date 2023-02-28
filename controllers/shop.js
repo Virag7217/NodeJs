5fb5fadffb6fb9f8b1e691d66bdda263f393b635
virag ranipa <virag.ranipa@seaflux.tech>
2023-02-28 15:49:25 +0530

    Pagination added

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,14 +3,31 @@ const Order = require("../models/order");
 const fs = require("fs");
 const path = require("path");
 const pdfKit = require("pdfkit");
+const itemsPerPage = 3;
 
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * itemsPerPage)
+        .limit(itemsPerPage);
+    })
     .then((products) => {
       res.render("shop/product-list", {
         prods: products,
-        pageTitle: "All Products",
+        pageTitle: "Products",
         path: "/products",
+        currentPage: page,
+        hasNextPage: itemsPerPage * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / itemsPerPage),
       });
     })
     .catch((err) => {
@@ -38,12 +55,28 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * itemsPerPage)
+        .limit(itemsPerPage);
+    })
     .then((products) => {
       res.render("shop/index", {
         prods: products,
         pageTitle: "Shop",
         path: "/",
+        currentPage: page,
+        hasNextPage: itemsPerPage * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / itemsPerPage),
       });
     })
     .catch((err) => {
